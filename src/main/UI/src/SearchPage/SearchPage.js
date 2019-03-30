1110af406405bdbb0b7119ef489cf3b5f87762f1
milesswu <mileswu13@gmail.com>
2019-03-30 13:44:20 -0700

    Update SearchPage.js

--- src/main/UI/src/SearchPage/SearchPage.js
+++ src/main/UI/src/SearchPage/SearchPage.js
@@ -6,6 +6,7 @@ class SearchPage extends React.Component {
     render() {
         return (
             <div id="parent">
+              <h1>Map</h1>
               <div id="inputSpace">
                 <form type = "text">
                     <label>
@@ -27,6 +28,8 @@ class SearchPage extends React.Component {
               </div>
               <div id="test" align="center">
                 <Link to="/map/123">Map Page</Link>
+                <br/>
+                <Link to="/leaderboards/123">Leaderboards Page</Link>
                 <br/>
                 <Link to="/stats/123">Stats Page</Link>
               </div>
