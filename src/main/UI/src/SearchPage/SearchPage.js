fb95f7d588150f549f83945e66f5130feb5f64b3
milesswu <mileswu13@gmail.com>
2019-03-30 11:32:46 -0700

    setup done

--- src/main/UI/src/SearchPage/SearchPage.js
+++ src/main/UI/src/SearchPage/SearchPage.js
@@ -5,6 +5,9 @@ class SearchPage extends React.Component {
     render() {
         return (
             <div id="parent">
+            <Link to="/map/123">Map Page</Link>
+            <br/>
+            <Link to="/stats/123">Stats Page</Link>
             </div>
         );
     }
