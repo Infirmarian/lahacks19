18631510ccbd8282c205ca608a791789fd4b9058
milesswu <mileswu13@gmail.com>
2019-03-31 00:35:02 -0700

    Update SearchPage.js

--- src/main/UI/src/SearchPage/SearchPage.js
+++ src/main/UI/src/SearchPage/SearchPage.js
@@ -97,20 +97,6 @@ class SearchPage extends React.Component {
                         <button id="submit" onClick={this.handleSubmit}>Search</button>
                     {/*</Link>*/}
               </div>
-              {/*<div id="inputSpace">
-                <form type = "text">
-                    <label>
-                        Search for Address
-                        <input 
-                            name="address" 
-                            id="autocomplete" 
-                            onFocus="geolocate()"
-                            placeholder="Enter Address"
-                        />
-                    </label>
-                    <Link to="/map/123"><button id="submit">Search</button></Link>
-                </form>              
-              </div>*/}
               <h1>LeaderBoards</h1>
               <div id="inputSpace">
                 <form type="text">
@@ -133,104 +119,5 @@ class SearchPage extends React.Component {
     }
 }
 
-class SearchAddress extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            address: '',
-            lat: 0,
-            long: 0
-        }
-        this.input = React.createRef();
-        this.autocomplete = null;
-        this.geocoder = new google.maps.Geocoder();
-        this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
-    componentDidMount() {
-        this.autocomplete = new google.maps.places.Autocomplete(
-          this.input.current,
-          { types: ["address"] }
-        );
-        this.autocomplete.addListener("place_changed", this.handlePlaceChanged);
-    }
-    
-    handlePlaceChanged() {
-        const place = this.autocomplete.getPlace();
-        this.input = place;
-        console.log(place);
-    }
-
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    } 
-
-    handleSubmit() {
-        if (this.input.formatted_address === '') {
-            return;
-        }
-
-        /*
-        if (!this.input.place_id) {
-            return;
-        }
-        console.log(this.input.geometry.location);
-        this.geocoder.geocode({'placeId': this.input.place_id}, function(results, status) {
-            console.log(results[0].geometry.location.lat);
-            if (status !== 'OK') {
-                window.alert('Geocoder failed due to: ' + status);
-                return;       
-            }
-            //console.log(results[0].geometry.location.lat. results[0].geometry.location.long)
-            /*
-            this.setState({
-                lat: results[0].geometry.location.lat, 
-                long: results[0].geometry.location.long,
-            });
-            
-        //})
-        //*/
-        console.log(this.input.formatted_address);
-        geocodeByAddress(this.input.formatted_address)
-        .then(results => getLatLng(results[0]))
-        .then(latLng => { this.setState({lat: latLng.lat, long: latLng.lng}) })//console.log('Success', latLng.lat, latLng.lng))//{ this.storeCoords(latLng) })//
-        .catch(error => console.error('Error', error));
-        
-        setTimeout( () => {
-            console.log(this.state.lat);
-            console.log(this.state.long);
-            this.props.history.push('/' + this.state.lat + '/' + this.state.long);
-        }, 500);
-        //*/
-    };
-
-    render() {
-        return (
-            <div id="parent">
-              <div id="inputSpace">
-                    <label>
-                        Search for Address
-                        <input 
-                            ref={this.input}
-                            value={this.state.address}
-                            name="address" 
-                            id="autocomplete" 
-                            placeholder="Enter Address"
-                            onChange={this.handleChange}
-                        />
-                    </label>
-                    {/*<Link to={"/map/" + this.state.lat + "/" + this.state.long}>*/}
-                        <button id="submit" onClick={this.handleSubmit}>Search</button>
-                    {/*</Link>*/}
-              </div>
-            </div>
-            
-        );
-    }
-    //*/
-}
-
-withRouter(SearchAddress);
+withRouter(SearchPage);
 export default SearchPage;
