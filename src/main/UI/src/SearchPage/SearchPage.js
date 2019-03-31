b3b9bc7a811aabd88604558a4f3fe35a81289493
milesswu <mileswu13@gmail.com>
2019-03-30 20:12:39 -0700

    latlng access

--- src/main/UI/src/SearchPage/SearchPage.js
+++ src/main/UI/src/SearchPage/SearchPage.js
@@ -49,14 +49,23 @@ class SearchPage extends React.Component {
 class SearchAddress extends React.Component {
     constructor(props) {
         super(props);
-        this.address = React.createRef();
+        this.state = {
+            address: '',
+            lat: 0,
+            long: 0
+        }
+        this.input = React.createRef();
         this.autocomplete = null;
+        this.geocoder = new google.maps.Geocoder();
         this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.storeCoords = this.storeCoords.bind(this);
     }
     
     componentDidMount() {
         this.autocomplete = new google.maps.places.Autocomplete(
-          this.address.current,
+          this.input.current,
           { types: ["address"] }
         );
         this.autocomplete.addListener("place_changed", this.handlePlaceChanged);
@@ -64,15 +73,48 @@ class SearchAddress extends React.Component {
     
     handlePlaceChanged() {
         const place = this.autocomplete.getPlace();
-        this.address = place;
+        this.input = place;
+        console.log(place);
     }
 
-    handleSubmit = address => {
-        geocodeByAddress(address)
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    } 
+
+    handleSubmit() {
+        /*
+        if (!this.input.place_id) {
+            return;
+        }
+        console.log(this.input.geometry.location);
+        this.geocoder.geocode({'placeId': this.input.place_id}, function(results, status) {
+            console.log(results[0].geometry.location.lat);
+            if (status !== 'OK') {
+                window.alert('Geocoder failed due to: ' + status);
+                return;       
+            }
+            //console.log(results[0].geometry.location.lat. results[0].geometry.location.long)
+            /*
+            this.setState({
+                lat: results[0].geometry.location.lat, 
+                long: results[0].geometry.location.long,
+            });
+            */
+        //})
+        
+        console.log(this.input.formatted_address);
+        geocodeByAddress(this.input.formatted_address)
         .then(results => getLatLng(results[0]))
-        .then(latLng => console.log('Success', latLng))
+        .then(latLng => console.log('Success', latLng.lat, latLng.lng))//{ this.storeCoords(latLng) })//this.setState({lat: latLng.lat, long: latLng.long}) })
         .catch(error => console.error('Error', error));
+        //*/
+        console.log(this.state.lat);
+        console.log(this.state.long);
     };
+
+    storeCoords(loc) {
+        this.setState({lat: loc.lat, long: loc.long});
+    }
 
     render() {
         return (
@@ -82,13 +124,15 @@ class SearchAddress extends React.Component {
                     <label>
                         Search for Address
                         <input 
-                            ref={this.address}
+                            ref={this.input}
+                            value={this.state.address}
                             name="address" 
                             id="autocomplete" 
                             placeholder="Enter Address"
+                            onChange={this.handleChange}
                         />
                     </label>
-                    <Link to="/map/123"><button id="submit" onClick={this.handleSubmit}>Search</button></Link>
+                    <Link to={"/map/" + this.state.lat + "-" + this.state.long}><button id="submit" onClick={this.handleSubmit}>Search</button></Link>
                 </form>
               </div>
             </div>
